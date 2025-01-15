25296fa41a5a57f1538dea05ebbc3dc20a26f1c1
MohitJaat8 <64859387+MohitJaat8@users.noreply.github.com>
2025-01-15 19:18:44 +0530

    Add files via upload

--- User.js
+++ User.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, required: true }, // Institute, Student, or Parent
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['student', 'parent', 'institute'],
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
